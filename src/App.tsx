a03e67a0ab594d144e441bf7db9fbc9b98f2627b
Stas Konashuk <skonashuk@list.ru>
2023-09-19 14:19:04 +0300

    feat: Add common components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+
+import { Button } from "snmt-button";
+import { Input } from "snmt-input";
+import { Select } from "snmt-select";
+
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [selectValue, setSelectValue] = useState("");
+
+  const selectOptions = [
+    {
+      value: "Option 1",
+      label: "Option 1",
+    },
+    {
+      value: "Option 2",
+      label: "Option 2",
+    },
+    {
+      value: "Option 3",
+      label: "Option 3",
+    },
+  ];
+
+  const onChangeHandler = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSelectChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target);
+    setSelectValue(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <Input
+          name="Name"
+          className="input"
+          value={inputValue}
+          width="300px"
+          onChange={onChangeHandler}
+          borderRadius="10px"
+        />
+        <Select
+          borderRadius="10px"
+          name="Option"
+          height="50px"
+          className="select"
+          value={selectValue}
+          options={selectOptions}
+          onChange={onSelectChangeHandler}
+        />
+
+        <Button borderRadius="10px" secondary value="Save" />
+      </form>
     </div>
   );
 }
